566c61f030fd5c191433df9b03991fe66b1b5269
Twan Huiskens <huiskenstwan@gmail.com>
2022-12-01 16:57:01 +0100

    Inital commit

---
+++ day00/part1.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def solution(s: str) -> int:
+    # TODO: implement solution here!
+    return 0
+
+
+INPUT_S = '''\
+
+'''
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, 1),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert solution(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(solution(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
